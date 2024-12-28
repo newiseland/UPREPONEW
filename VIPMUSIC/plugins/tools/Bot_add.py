bf2bc08ee64ae26611e022010acfc1bb77f863b2
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-28 11:13:13 +0530

    Update Bot_add.py

--- VIPMUSIC/plugins/tools/Bot_add.py
+++ VIPMUSIC/plugins/tools/Bot_add.py
@@ -3043,7 +3043,7 @@ photo = [
 @app.on_message(filters.new_chat_members, group=-9)
 async def join_watcher(_, message):
     try:
-        LOG = "@MAGICIAN_CHATBOT"
+        LOG = "@SNAYA_ROBOT"
         userbot = await get_assistant(message.chat.id)
         chat = message.chat
         for members in message.new_chat_members:
